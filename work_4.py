d27d5101c741e20b190f00c5e89d7ed259e8cd5a
kasym0803 <kasymbek618618@gmail.com>
2023-08-19 23:37:26 +0300

    homework_4

---
+++ work_4.py
@@ -0,0 +1,34 @@
+
+
+class klass_1:
+    def __init__(self,name):
+        self.name = name
+
+    def __str__(self):
+        return f'name:{self.name}'
+
+
+class klass_2:
+    def __init__(self,age):
+        self.age = age
+
+    def __str__(self):
+        return f'age:{self.age}'
+    
+
+class klass_3:
+
+    def method_1(self):
+        print('метод_1')
+
+
+class klass_4:
+
+    def method_2(self):
+        print('метод_2')
+
+
+
+
+
+    
